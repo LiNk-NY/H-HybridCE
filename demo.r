a8e403aa48f7e04967e3f1e1cdac857d0784a987
LiNk-NY <mramos09@gmail.com>
2014-07-21 16:04:36 -0400

    Data Cleaning

--- demo.r
+++ demo.r
@@ -6,14 +6,14 @@ getwd()
 dir()
 demo <- read.csv("~/Fieldwork SPH/Data/demographics.csv", header=TRUE)
 demo <- demo[,c("Gender", "Age.group", "Percentage")]
-demo$percentage <- as.numeric(unlist(strsplit(as.character(demo$Percentage), split="0000%", fixed=TRUE)))
+demo$Percentage <- as.numeric(unlist(strsplit(as.character(demo$Percentage), split="0000%", fixed=TRUE)))
 summary(demo$Age.group)
 demo$AgeG <- factor(demo$Age.group, levels=c("18-24", "25-34", "35-44", "45-54", "55-64", "65-"))
 demo
 
 library(lattice)
 png("Demo_by_AG.png")
-barchart(demo$percentage~demo$AgeG, data=demo, groups=demo$Gender, auto.key=list(space="right"), main="Demographics by Age Group", xlab="Age Group", ylab="Percentage")
+barchart(demo$Percentage~demo$AgeG, data=demo, groups=demo$Gender, auto.key=list(space="right"), main="Demographics by Age Group", xlab="Age Group", ylab="Percentage")
 graphics.off()
 
 devs <- read.csv("~/Fieldwork SPH/Data/devices.csv", header=TRUE)
@@ -23,15 +23,23 @@ devs
 library(xlsx)
 cols <- 1:44
 cols <- cols[-c(13,17,30,33,35,36)]
-survey <- read.xlsx("~/Fieldwork SPH/Data/PH750-2 online tech 07-09.xlsx", sheetIndex=1, header=TRUE, colIndex=cols,
-                    colClasses=c("character", "character", rep("numeric",4),"character","character",rep("numeric",4),rep("character",5),"numeric",
-                                 "character","character","numeric","numeric",rep("character",4),"numeric", "character","character","numeric",
-                                 rep("character",6),"numeric","numeric") )
+
 colnamesfile <- read.csv("~/Fieldwork SPH/Data/Codebook1.csv", header=FALSE, colClasses=c("character", "character"), na.strings="NA")
+
+survey <- read.xlsx("~/Fieldwork SPH/Data/PH750-2 online tech 07-09.xlsx", sheetIndex=1, header=TRUE, colIndex=cols, colClasses=NA)
+
 compl <- complete.cases(colnamesfile)
-colnames(survey) <- colnamesfile$V2[compl==TRUE]
+colnames(survey) <- colnamesfile$V2[compl]
+chars <- c(8,15,32,33,35,36)
+for (i in chars) {
+        class(survey[,i]) <- "character"
+}
+
+sapply(survey,class)
+
 names(survey)
 head(survey,10)
+
 table(survey$EpiBiosStatus)
 
 table(survey$MedPrefBIOS)
