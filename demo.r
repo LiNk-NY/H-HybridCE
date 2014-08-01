bb1237bb260942691cf495b4958c874c6f12e0b5
LiNk-NY <mramos09@gmail.com>
2014-07-31 22:06:50 -0400

    Adding figure 1 and code for tables

--- demo.r
+++ demo.r
@@ -26,8 +26,7 @@ cols <- cols[-c(13,17,30,33,35,36)]
 
 colnamesfile <- read.csv("~/Fieldwork SPH/Data/Codebook1.csv", header=FALSE, colClasses=c("character", "character"), na.strings="NA")
 
-survey <- read.xlsx("~/Fieldwork SPH/Data/PH750-2 online tech 07-09.xlsx", sheetIndex=1, header=TRUE, colIndex=cols, colClasses=NA)
-
+survey <- read.xlsx("~/Fieldwork SPH/Data/PH750-2 online tech 07-09.xlsx", sheetIndex=1, header=TRUE, colIndex=cols, colClasses=NA, stringsAsFactors=FALSE)
 compl <- complete.cases(colnamesfile)
 colnames(survey) <- colnamesfile$V2[compl]
 chars <- c(8,15,32,33,35,36)
@@ -39,10 +38,23 @@ sapply(survey,class)
 
 names(survey)
 head(survey,10)
+summary(survey)
 
+survey$EpiBiosStatus[1] <- "PH750"
+survey$EpiBiosStatus[24] <- "PH750"
+survey$EpiBiosStatus <- factor(survey$EpiBiosStatus, levels=c("PH750", "PH752"))
 table(survey$EpiBiosStatus)
 
-table(survey$MedPrefBIOS)
+
+
+table(survey$MedPrefBIOS, survey$EpiBiosStatus)
+table(survey$MedPrefEPI, survey$EpiBiosStatus)
+
+#Matrix of preferences
+prefs <- matrix(c(12,6,9,5,5,0), ncol=2, byrow=TRUE)
+rownames(prefs) <- c("In person", "Online after class", "Online during class")
+colnames(prefs) <- c("Bios", "Epi")
+prefs
 
 mean(survey$Travelminutes)
 sd(survey$Travelminutes)
@@ -53,8 +65,29 @@ points(survey$Travelminutes, pch=0)
 abline(h=mean(survey$Travelminutes), lty=3)
 abline(h=100.1622-65.80665, lty=4)
 abline(h=100.1622+65.80665, lty=4)
-legend("top", legend=c("Both", "Biostatistics","Epidemiology"), col=c("black", "red", "green"), lty=1)
+legend("top", legend=c("Biostatistics","Epidemiology"), col=c("black","red"), lty=1)
 graphics.off()
 
 boxplot(survey$Travelminutes, type="h")
 
+#Reason for preferences
+!is.na(survey$MedPrefRBIOS)
+!is.na(survey$PrefEPIR)
+
+Reasons <- ifelse(!is.na(survey$MedPrefRBIOS)==TRUE, survey$MedPrefRBIOS, survey$PrefEPIR)
+RC1 <- c("Interactivity", "Interactivity", NA, "Convenience", "Interactivity", "AvoidCommute", "LearnPref", "Convenience", "LearnPref", "LearnPref",
+         "Interactivity", "LearnPref", "Interactivity", "Convenience", "AvoidCommute", NA, "AvoidCommute", NA, "Interactivity", "LearnPref",
+         "LearnPref", "Convenience", "Convenience", "AvoidCommute", "LearnPref", "Interactivity", "Convenience", "Interactivity", "AvoidCommute", "LearnPref",
+         "Convenience", "LearnPref", "AvoidCommute", "LearnPref", "LearnPref", "LearnPref", "LearnPref")
+RC1 <- factor(RC1, levels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"), labels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"))
+
+table(RC1, survey$EpiBiosStatus)
+
+RC2 <- c("LearnPref", NA, NA, NA, NA, "Interactivity", NA, NA, "AvoidCommute", NA, 
+         NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 
+         NA, NA, NA, "Interactivity", NA, NA, NA, NA, "Convenience", NA, 
+         NA, NA, "Convenience", NA, "Interactivity", NA, "Convenience" )
+RC2 <- factor(RC2,levels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"), labels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"))
+#ReasonsCat <- data.frame(cbind(Reasons, RC1, RC2))
+table(RC2, survey$EpiBiosStatus)
+                                                                                                                                                                                                                                          
