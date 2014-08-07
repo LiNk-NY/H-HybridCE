7d4b0162d65b30da7bdf5042cd4f1ca3230665ca
LiNk-NY <mramos09@gmail.com>
2014-08-07 00:41:51 -0400

    updated demo file with tests

--- demo.r
+++ demo.r
@@ -1,9 +1,7 @@
 ##Marcel Ramos
 ##YouTube viewing habits
 
-setwd("~/Fieldwork SPH/GitHub/H-HybridCE/")
-getwd()
-dir()
+setwd("~/GitHub/H-HybridCE/")
 demo <- read.csv("~/Fieldwork SPH/Data/demographics.csv", header=TRUE)
 demo <- demo[,c("Gender", "Age.group", "Percentage")]
 demo$Percentage <- as.numeric(unlist(strsplit(as.character(demo$Percentage), split="0000%", fixed=TRUE)))
@@ -12,9 +10,7 @@ demo$AgeG <- factor(demo$Age.group, levels=c("18-24", "25-34", "35-44", "45-54",
 demo
 
 library(lattice)
-png("Demo_by_AG.png")
 barchart(demo$Percentage~demo$AgeG, data=demo, groups=demo$Gender, auto.key=list(space="right"), main="Demographics by Age Group", xlab="Age Group", ylab="Percentage")
-graphics.off()
 
 devs <- read.csv("~/Fieldwork SPH/Data/devices.csv", header=TRUE)
 devs$devper <- round(devs$Views/sum(devs$Views)*100, 2)
@@ -34,27 +30,31 @@ for (i in chars) {
         class(survey[,i]) <- "character"
 }
 
-sapply(survey,class)
-
-names(survey)
-head(survey,10)
 summary(survey)
 
 survey$EpiBiosStatus[1] <- "PH750"
 survey$EpiBiosStatus[24] <- "PH750"
-survey$EpiBiosStatus <- factor(survey$EpiBiosStatus, levels=c("PH750", "PH752"))
+survey$EpiBiosStatus <- factor(survey$EpiBiosStatus, labels=c("Bios", "Epi"))
+
+#Totals
 table(survey$EpiBiosStatus)
 
-
-
-table(survey$MedPrefBIOS, survey$EpiBiosStatus)
+#Method/Medium Preference by Class
+table(survey$MedPrefBIOS, survey$EpiBiosStatus) 
 table(survey$MedPrefEPI, survey$EpiBiosStatus)
 
-#Matrix of preferences
+#Matrix of preferences #assuming online for PH752 == Online after class is over
 prefs <- matrix(c(12,6,9,5,5,0), ncol=2, byrow=TRUE)
 rownames(prefs) <- c("In person", "Online after class", "Online during class")
 colnames(prefs) <- c("Bios", "Epi")
 prefs
+
+#Course format choice if participant were to re-take course in the future
+Hchoice <- table(survey$FFormatThis, survey$EpiBiosStatus)
+colSums(Hchoice)
+OtherChoice <- table(survey$FFormatOther, survey$EpiBiosStatus)
+colSums(OtherChoice)
+
 
 mean(survey$Travelminutes)
 sd(survey$Travelminutes)
@@ -79,15 +79,52 @@ RC1 <- c("Interactivity", "Interactivity", NA, "Convenience", "Interactivity", "
          "Interactivity", "LearnPref", "Interactivity", "Convenience", "AvoidCommute", NA, "AvoidCommute", NA, "Interactivity", "LearnPref",
          "LearnPref", "Convenience", "Convenience", "AvoidCommute", "LearnPref", "Interactivity", "Convenience", "Interactivity", "AvoidCommute", "LearnPref",
          "Convenience", "LearnPref", "AvoidCommute", "LearnPref", "LearnPref", "LearnPref", "LearnPref")
-RC1 <- factor(RC1, levels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"), labels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"))
-
-table(RC1, survey$EpiBiosStatus)
+survey$RC1 <- factor(RC1, levels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"), labels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"))
 
 RC2 <- c("LearnPref", NA, NA, NA, NA, "Interactivity", NA, NA, "AvoidCommute", NA, 
          NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 
          NA, NA, NA, "Interactivity", NA, NA, NA, NA, "Convenience", NA, 
          NA, NA, "Convenience", NA, "Interactivity", NA, "Convenience" )
-RC2 <- factor(RC2,levels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"), labels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"))
-#ReasonsCat <- data.frame(cbind(Reasons, RC1, RC2))
-table(RC2, survey$EpiBiosStatus)
-                                                                                                                                                                                                                                          
+survey$RC2 <- factor(RC2,levels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"), labels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"))
+
+table(survey$Race)
+library(plyr)
+survey$Race2 <- revalue(survey$Race, c("Asian"="Other", "Prefer not to respond"="Other", "South Asian/European Jew"="Other"))
+survey$Race2 <- factor(survey$Race2, levels=c("Other", "Hispanic", "Non-Hispanic black", "Non-Hispanic white"))
+
+survey$Gender[11] <- NA
+survey$Gender <- factor(survey$Gender, levels=c("Female", "Male"))
+table(survey$Gender, survey$EpiBiosStatus)
+table(survey$Race2, survey$EpiBiosStatus)
+by(survey$Age, survey$EpiBiosStatus, FUN=function(x) {c(m = mean(x, na.rm=TRUE), s = sd(x, na.rm=TRUE))})
+by(survey$Travelminutes, survey$EpiBiosStatus, FUN=function(x) {c(m = mean(x, na.rm=TRUE), s = sd(x, na.rm=TRUE))})
+
+#Subsetting for BIOS students
+biosub <- subset(survey, survey$EpiBiosStatus=="Bios")
+nrow(biosub)
+
+#barplot(comb, horiz=TRUE, col=gray.colors(4),beside=TRUE, legend.text=rownames(comb), main="Reasons for Medium Preference", xlab="Frequency Endorsed")
+
+#Primary and secondary reason for course pref selection
+comb <-  table(biosub$RC1, biosub$MedPrefBIOS) + table(biosub$RC2, biosub$MedPrefBIOS)
+comb
+colnames(comb) <- c("In-person", "Synchronous", "Asynchronous")
+rownames(comb) <- c("Interactivity", "Convenience", "Avoid Commuting", "Learning Preference")
+comb
+
+#Figure 2
+barplot(comb, col=gray.colors(4), beside=TRUE, legend.text=rownames(comb), main="Reasons for Course Format Preference", xlab="Course Format Preferences", ylab="Frequency Endorsed") 
+
+#Age by Preference
+summary( with(biosub, aov(Age~ MedPrefBIOS, data=biosub)))
+#Travel Time by Preference
+summary( with(biosub, aov(Travelminutes~ MedPrefBIOS, data=biosub)))
+#Gender by Preference
+with(biosub, fisher.test(Gender, MedPrefBIOS))
+#Race by Preference
+with(biosub, fisher.test(Race2, MedPrefBIOS))
+#Pref Reason by Preference
+with(biosub, fisher.test(RC1, MedPrefBIOS))
+
+
+summary(with(biosub, aov(Travelminutes~ RC1, data=biosub)))
