ac9c754d75b98a076080a249b3f0e702a0602eb1
LiNk-NY <mramos09@gmail.com>
2014-08-11 20:08:36 -0400

    Updating file and graphic

--- demo.r
+++ demo.r
@@ -1,7 +1,7 @@
 ##Marcel Ramos
 ##YouTube viewing habits
 
-setwd("~/GitHub/H-HybridCE/")
+setwd("~/Fieldwork SPH/GitHub/H-HybridCE/")
 demo <- read.csv("~/Fieldwork SPH/Data/demographics.csv", header=TRUE)
 demo <- demo[,c("Gender", "Age.group", "Percentage")]
 demo$Percentage <- as.numeric(unlist(strsplit(as.character(demo$Percentage), split="0000%", fixed=TRUE)))
@@ -68,7 +68,7 @@ abline(h=100.1622+65.80665, lty=4)
 legend("top", legend=c("Biostatistics","Epidemiology"), col=c("black","red"), lty=1)
 graphics.off()
 
-boxplot(survey$Travelminutes, type="h")
+#boxplot(survey$Travelminutes, type="h")
 
 #Reason for preferences
 !is.na(survey$MedPrefRBIOS)
@@ -87,6 +87,9 @@ RC2 <- c("LearnPref", NA, NA, NA, NA, "Interactivity", NA, NA, "AvoidCommute", N
          NA, NA, "Convenience", NA, "Interactivity", NA, "Convenience" )
 survey$RC2 <- factor(RC2,levels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"), labels=c("Interactivity", "Convenience", "AvoidCommute", "LearnPref"))
 
+#checking reason labels - Primary Reason, BIOS Reasons, EPI Reasons, Secondary Reason 
+with(survey, paste(RC1, Reasons, RC2)) 
+
 table(survey$Race)
 library(plyr)
 survey$Race2 <- revalue(survey$Race, c("Asian"="Other", "Prefer not to respond"="Other", "South Asian/European Jew"="Other"))
@@ -94,8 +97,8 @@ survey$Race2 <- factor(survey$Race2, levels=c("Other", "Hispanic", "Non-Hispanic
 
 survey$Gender[11] <- NA
 survey$Gender <- factor(survey$Gender, levels=c("Female", "Male"))
-table(survey$Gender, survey$EpiBiosStatus)
-table(survey$Race2, survey$EpiBiosStatus)
+table(survey$Gender, survey$EpiBiosStatus); prop.table(table(survey$Gender, survey$EpiBiosStatus), 2)
+table(survey$Race2, survey$EpiBiosStatus); prop.table(table(survey$Race2, survey$EpiBiosStatus),2)
 by(survey$Age, survey$EpiBiosStatus, FUN=function(x) {c(m = mean(x, na.rm=TRUE), s = sd(x, na.rm=TRUE))})
 by(survey$Travelminutes, survey$EpiBiosStatus, FUN=function(x) {c(m = mean(x, na.rm=TRUE), s = sd(x, na.rm=TRUE))})
 
@@ -112,19 +115,33 @@ colnames(comb) <- c("In-person", "Synchronous", "Asynchronous")
 rownames(comb) <- c("Interactivity", "Convenience", "Avoid Commuting", "Learning Preference")
 comb
 
-#Figure 2
+#Figure 2 - use pdf() for final graphic
+png("PrefReasons.png")
 barplot(comb, col=gray.colors(4), beside=TRUE, legend.text=rownames(comb), main="Reasons for Course Format Preference", xlab="Course Format Preferences", ylab="Frequency Endorsed") 
-
+graphics.off()
 #Age by Preference
 summary( with(biosub, aov(Age~ MedPrefBIOS, data=biosub)))
+with(biosub, by(Age, MedPrefBIOS, FUN=function(x) {c(m = mean(x, na.rm=TRUE), sd = sd(x, na.rm=TRUE))}) )
 #Travel Time by Preference
 summary( with(biosub, aov(Travelminutes~ MedPrefBIOS, data=biosub)))
+with(biosub, by(Travelminutes, MedPrefBIOS, FUN=function(x) {c(m = mean(x, na.rm=TRUE), sd = sd(x, na.rm=TRUE))}))
 #Gender by Preference
 with(biosub, fisher.test(Gender, MedPrefBIOS))
+with(biosub, xtabs(~Gender+MedPrefBIOS))
+#percentages by row
+prop.table(with(biosub, xtabs(~Gender+MedPrefBIOS)),1)
+
 #Race by Preference
 with(biosub, fisher.test(Race2, MedPrefBIOS))
-#Pref Reason by Preference
-with(biosub, fisher.test(RC1, MedPrefBIOS))
+with(biosub, xtabs(~Race2+MedPrefBIOS))
+#table with row percentages
+prop.table(with(biosub, xtabs(~Race2+MedPrefBIOS)),1)
 
+#Pref Reason by Preference
+with(biosub, fisher.test(RC1, MedPrefBIOS))$p.value
+with(biosub, xtabs(~RC1+MedPrefBIOS))
+#column percentages
+prop.table(with(biosub, xtabs(~RC1+MedPrefBIOS)),2)
 
 summary(with(biosub, aov(Travelminutes~ RC1, data=biosub)))
+
