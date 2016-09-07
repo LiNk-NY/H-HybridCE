cd0e5f864a102ae9de90df280d0eefc7437bf838
LiNk-NY <mramos09@gmail.com>
2016-09-07 15:45:44 -0400

    clean up code

--- demo.r
+++ demo.r
@@ -15,19 +15,20 @@ devs <- read.csv("data-raw/devices.csv", header=TRUE)
 devs
 devs$devper <- round(devs$Views/sum(devs$Views)*100, 2)
 
-
 library(xlsx)
+
 survey <- read.xlsx("data-raw/PH750-2 online tech 07-09.xlsx", sheetIndex=1, header=TRUE)
-head(survey,10)
+
 table(survey$What.was.your.preferred.way.to.attend.lectures.for.this.course.)
+
+## Exploratory Graphs
 plot(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class.., type="h", pch=0)
 points(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class.., pch=0)
-abline(h=mean(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class..), lty=3)
-abline(h=100.1622-65.80665, lty=4)
-abline(h=100.1622+65.80665, lty=4)
+meanMinutes <- mean(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class..)
+sdMinutes <- sd(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class..)
+abline(h=meanMinutes, lty=3)
+abline(h=meanMinutes - sdMinutes, lty=4)
+abline(h=meanMinutes + sdMinutes, lty=4)
 
 boxplot(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class.., type="h")
 
-mean(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class..)
-sd(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class..)
-sd(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class..)
