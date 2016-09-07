66eec39da7af75a986b76d1d123b5185cea392d0
LiNk-NY <mramos09@gmail.com>
2016-09-07 15:34:03 -0400

    Point to data folder

--- demo.r
+++ demo.r
@@ -11,15 +11,15 @@ demo
 library(lattice)
 barchart(demo$percentage~demo$AgeG, data=demo, groups=demo$Gender, auto.key=list(space="right"), main="Demographics by Age Group", xlab="Age Group", ylab="Percentage")
 
-devs <- read.csv("Data/devices.csv", header=TRUE)
+devs <- read.csv("data-raw/devices.csv", header=TRUE)
 devs
 devs$devper <- round(devs$Views/sum(devs$Views)*100, 2)
 
 
 library(xlsx)
-survey <- read.xlsx("Data/PH750-2 online tech 07-09.xlsx", sheetIndex=1, header=TRUE)
+survey <- read.xlsx("data-raw/PH750-2 online tech 07-09.xlsx", sheetIndex=1, header=TRUE)
 head(survey,10)
-survey$What.was.your.preferred.way.to.attend.lectures.for.this.course.
+table(survey$What.was.your.preferred.way.to.attend.lectures.for.this.course.)
 plot(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class.., type="h", pch=0)
 points(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class.., pch=0)
 abline(h=mean(survey$How.many.minutes.of.total.travel.time.does.it.take.for.you.to.attend.a.class.on.campus..including.time.spent.both.getting.to.and.from.class..), lty=3)
